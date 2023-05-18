c55d77621e61b51320128da1f1051d2a7b29126c
strukovd <dmitryi97@yandex.ru>
2023-05-18 19:59:13 +0600

    start adding routing.. and nest modules

--- frontend/vue-spa-vkr/src/main.js
+++ frontend/vue-spa-vkr/src/main.js
@@ -1,5 +1,24 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue';
 import './styles/index.css';
 
-createApp(App).mount('#app');
+const router = createRouter({
+	history: createWebHashHistory(),
+	routes: [
+		{
+			path: "/",
+			component: ""
+		},
+		{
+			path: "/start",
+			component: ""
+		}
+		// здесь должны быть определены маршруты
+	]
+});
+
+
+createApp(App)
+	.use(router)
+	.mount('#app');
