54def06650c0f2c2b52a9f43b7e522b2d0f8a454
strukovd <dmitryi97@yandex.ru>
2023-06-08 01:51:24 +0600

    backup forms

--- postgresql/forms-backup.sh
+++ postgresql/forms-backup.sh
@@ -4,4 +4,4 @@ source ../.env
 
 # docker exec -t ${PG_CONTAINER_NAME} pg_dumpall -c -U ${PG_USERNAME} > pg_dump.sql #dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
 
-docker exec -t ${PG_CONTAINER_NAME} pg_dumpall -c -U ${PG_USERNAME} > dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+docker exec -t ${PG_CONTAINER_NAME} pg_dumpall -c -U ${PG_USERNAME} > forms_dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
