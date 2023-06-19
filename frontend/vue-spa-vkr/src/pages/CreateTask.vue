040a85497ce0f0bf017512b6ebff368556d98209
strukovd <dmitryi97@yandex.ru>
2023-06-19 14:16:38 +0600

    some fix vue and forms backup file

--- frontend/vue-spa-vkr/src/pages/CreateTask.vue
+++ frontend/vue-spa-vkr/src/pages/CreateTask.vue
@@ -1,33 +1,35 @@
 <template>
-	<div class="create-task-block">
-		<h1 class="title">Создание задачи</h1>
-		<form>
-			<table>
-				<tr>
-					<td><span class="label">Проект:</span></td>
-					<td><span><ProjectSelect v-model="projectKey"/></span></td>
-				</tr>
-				<tr>
-					<td><span class="label">Заголовок:</span></td>
-					<td><span><input v-model="title" type="text"></span></td>
-				</tr>
-				<tr>
-					<td><span class="label">Описание:</span></td>
-					<td><span><textarea v-model="description" rows="10"></textarea></span></td>
-				</tr>
-				<tr>
-					<td><span class="label">Приоритет:</span></td>
-					<td><span><PrioritySelect v-model="priority"/></span></td>
-				</tr>
-				<tr>
-					<td><span class="label">Исполнитель:</span></td>
-					<td><span><UserSelect v-model="assignee"/></span></td>
-				</tr>
-				<tr>
-					<td colspan="2"><input @click.prevent="sendForm" type="submit"></td>
-				</tr>
-			</table>
-		</form>
+	<div class="wr-create-task-block">
+		<div class="create-task-block">
+			<h1 class="title">Создание задачи</h1>
+			<form>
+				<table>
+					<tr>
+						<td><span class="label">Проект:</span></td>
+						<td><span><ProjectSelect v-model="projectKey"/></span></td>
+					</tr>
+					<tr>
+						<td><span class="label">Заголовок:</span></td>
+						<td><span><input v-model="title" type="text"></span></td>
+					</tr>
+					<tr>
+						<td><span class="label">Описание:</span></td>
+						<td><span><textarea v-model="description" rows="10"></textarea></span></td>
+					</tr>
+					<tr>
+						<td><span class="label">Приоритет:</span></td>
+						<td><span><PrioritySelect v-model="priority"/></span></td>
+					</tr>
+					<tr>
+						<td><span class="label">Исполнитель:</span></td>
+						<td><span><UserSelect v-model="assignee"/></span></td>
+					</tr>
+					<tr>
+						<td colspan="2"><input @click.prevent="sendForm" type="submit"></td>
+					</tr>
+				</table>
+			</form>
+		</div>
 	</div>
 </template>
 
@@ -78,86 +80,91 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-	.create-task-block {
-		margin: 1em;
-		background: white;
-		padding: 4em 12em;
-		text-align: center;
-		border-radius: 3px;
-		box-shadow: 0px 1px 4px 0 rgba(0, 0, 0, 0.1);
+	.wr-create-task-block {
+		display: flex;
 
-		table {
-			width: 100%;
-			max-width: 600px;
-			margin: auto;
+		.create-task-block {
+			margin: 4em auto auto auto;
+			background: white;
+			padding: 5em 4em;
+			flex: 600px 0 1;
+			text-align: center;
+			border-radius: 3px;
+			box-shadow: 0px 1px 4px 0 rgba(0, 0, 0, 0.1);
 
-			tr:not(:last-child) {
-				td:first-child {
-					text-align: right;
-					padding-right: 1em;
-				}
+			table {
+				width: 100%;
+				max-width: 600px;
+				margin: auto;
 
-				td:last-child {
-					* {
-						width: 100%;
+				tr:not(:last-child) {
+					td:first-child {
+						text-align: right;
+						padding-right: 1em;
+					}
+
+					td:last-child {
+						* {
+							width: 100%;
+						}
 					}
 				}
 			}
-		}
 
-		select {
-			box-sizing: border-box;
-			background: #f5f5f5;
-			color: #172b4dba;
-			font-size: 16px;
-			padding: 0 1em;
-			line-height: 2em;
-			height: 2em;
-			border-radius: 5px;
-			outline: none;
-			border: none;
-		}
+			select {
+				box-sizing: border-box;
+				background: #f5f5f5;
+				color: #172b4dba;
+				font-size: 16px;
+				padding: 0 1em;
+				line-height: 2em;
+				height: 2em;
+				border-radius: 5px;
+				outline: none;
+				border: none;
+			}
 
-		input[type=text] {
-			box-sizing: border-box;
-			background: #f5f5f5;
-			color: #172b4dba;
-			font-size: 16px;
-			padding: 0 1em;
-			line-height: 2em;
-			height: 2em;
-			border-radius: 5px;
-			outline: none;
-			border: none;
-		}
+			input[type=text] {
+				box-sizing: border-box;
+				background: #f5f5f5;
+				color: #172b4dba;
+				font-size: 16px;
+				padding: 0 1em;
+				line-height: 2em;
+				height: 2em;
+				border-radius: 5px;
+				outline: none;
+				border: none;
+			}
 
-		input[type=submit] {
-			margin-top:.5em;
-			box-sizing: border-box;
-			width: 100%;
-			background: #0079c1;
-			color: #f0f0f0;
-			font-weight: 700;
-			font-size: 18px;
-			padding: 0 1em;
-			line-height: 2em;
-			border-radius: 4px;
-			outline: none;
-			border: none;
-			cursor: pointer;
-		}
+			input[type=submit] {
+				margin-top:.5em;
+				box-sizing: border-box;
+				width: 100%;
+				background: #0079c1;
+				color: #f0f0f0;
+				font-weight: 700;
+				font-size: 18px;
+				padding: 0 1em;
+				line-height: 2em;
+				border-radius: 4px;
+				outline: none;
+				border: none;
+				cursor: pointer;
+			}
 
-		textarea {
-			box-sizing: border-box;
-			background: #f5f5f5;
-			color: #172b4dba;
-			font-size: 16px;
-			padding: 0 1em;
-			// line-height: 2em;
-			// height: 2em;
-			border-radius: 5px;
-			outline: none;
-			border: none;
+			textarea {
+				box-sizing: border-box;
+				background: #f5f5f5;
+				color: #172b4dba;
+				font-size: 16px;
+				padding: 0 1em;
+				// line-height: 2em;
+				// height: 2em;
+				border-radius: 5px;
+				outline: none;
+				border: none;
+			}
 		}
 	}
 </style>
--- frontend/vue-spa-vkr/src/pages/LoginPage.vue
+++ frontend/vue-spa-vkr/src/pages/LoginPage.vue
@@ -1,21 +1,23 @@
 <template>
-	<div class="auth-block">
-		<h1 class="title">Авторизация</h1>
-		<form>
-			<table>
-				<tr>
-					<td><span class="fieldLabel">Логин:</span></td>
-					<td><input v-model="username" type="text"/></td>
-				</tr>
-				<tr>
-					<td><span class="fieldLabel">Пароль:</span></td>
-					<td><input v-model="password" type="password"/></td>
-				</tr>
-				<tr>
-					<td colspan="2"><input @click.prevent="signin" type="submit" value="Войти"></td>
-				</tr>
-			</table>
-		</form>
+	<div class="wr-auth-block">
+		<div class="auth-block">
+			<h1 class="title">Авторизация</h1>
+			<form>
+				<table>
+					<tr>
+						<td><span class="fieldLabel">Логин:</span></td>
+						<td><input v-model="username" type="text"/></td>
+					</tr>
+					<tr>
+						<td><span class="fieldLabel">Пароль:</span></td>
+						<td><input v-model="password" type="password"/></td>
+					</tr>
+					<tr>
+						<td colspan="2"><input @click.prevent="signin" type="submit" value="Войти"></td>
+					</tr>
+				</table>
+			</form>
+		</div>
 	</div>
 </template>
 
@@ -61,52 +63,56 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-	.auth-block {
-		margin: 1em;
-		background: white;
-		padding: 4em 12em;
-		text-align: center;
-		border-radius: 3px;
-		box-shadow: 0px 1px 4px 0 rgba(0, 0, 0, 0.1);
+	.wr-auth-block {
+		display: flex;
 
-		.title {
-			color: #172b4d;
-		}
+		.auth-block {
+			margin: 2em auto auto;
+			background: white;
+			padding: 4em 12em;
+			text-align: center;
+			border-radius: 3px;
+			box-shadow: 0px 1px 4px 0 rgba(0, 0, 0, 0.1);
 
-		form {
-			table {
-				margin: auto;
-				width: 100%;
+			.title {
+				color: #172b4d;
+			}
 
-				tr {
-					td{
-						padding-bottom:.5em;
-					}
+			form {
+				table {
+					margin: auto;
+					width: 100%;
 
-					input[type=text], input[type=password] {
-						box-sizing: border-box;
-						background: #f5f5f5;
-						color: #172b4dba;
-						font-size: 18px;
-						padding: 0 1em;
-						line-height: 2em;
-						border-radius: 5px;
-						outline: none;
-						border: none;
-					}
+					tr {
+						td{
+							padding-bottom:.5em;
+						}
 
-					input[type=submit] {
-						box-sizing: border-box;
-						width: 100%;
-						background: #0079c1;
-						color: #f0f0f0;
-						font-weight: 700;
-						font-size: 18px;
-						padding: 0 1em;
-						line-height: 2em;
-						border-radius: 4px;
-						outline: none;
-						border: none;
+						input[type=text], input[type=password] {
+							box-sizing: border-box;
+							background: #f5f5f5;
+							color: #172b4dba;
+							font-size: 18px;
+							padding: 0 1em;
+							line-height: 2em;
+							border-radius: 5px;
+							outline: none;
+							border: none;
+						}
+
+						input[type=submit] {
+							box-sizing: border-box;
+							width: 100%;
+							background: #0079c1;
+							color: #f0f0f0;
+							font-weight: 700;
+							font-size: 18px;
+							padding: 0 1em;
+							line-height: 2em;
+							border-radius: 4px;
+							outline: none;
+							border: none;
+						}
 					}
 				}
 			}
